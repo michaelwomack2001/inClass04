abe3cb0dd30df2685ddaf8a3866eca5ee69489b2
Linh <thung2@uncc.edu>
2022-09-13 09:18:35 -0400

    registration

--- app/src/main/java/com/example/inclass04/MainActivityFragment.java
+++ app/src/main/java/com/example/inclass04/MainActivityFragment.java
@@ -52,6 +52,7 @@ public class MainActivityFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
